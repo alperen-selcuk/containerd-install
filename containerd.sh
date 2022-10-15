1a8414eff362ec830cd881a54a8c7547034526f0
Hasan Alperen <78741582+alperen-selcuk@users.noreply.github.com>
2022-10-15 12:02:04 +0300

    Create containerd.sh

---
+++ containerd.sh
@@ -0,0 +1,10 @@
+!#/bin/bash
+
+sudo su -
+apt-get update
+apt-get install containerd -y
+mkdir -p /etc/containerd
+containerd config default /etc/containerd/config.toml
+
+systemctl restart containerd
+systemctl enable containerd
